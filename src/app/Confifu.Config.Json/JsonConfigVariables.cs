eee0885f4f462b69af326c582b6833686b229196
ivanbenko <bivans209@gmail.com>
2017-02-22 15:38:24 +0100

    initial

---
+++ src/app/Confifu.Config.Json/JsonConfigVariables.cs
@@ -0,0 +1,111 @@
+﻿using Confifu.Abstractions;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Confifu.Config.Json
+{
+    public class JsonConfigVariables : IConfigVariables
+    {
+        private readonly string jsonContent;
+        private readonly Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        public JsonConfigVariables(string jsonContent)
+        {
+            if (jsonContent == null) throw new ArgumentNullException(nameof(jsonContent));
+            this.jsonContent = jsonContent;
+
+            Load();
+        }
+
+        private void Load()
+        {
+            var json = JObject.Parse(jsonContent);
+
+            ReadJsonRec(json);
+        }
+
+        private void ReadJsonRec(JObject json, string prefix = "")
+        {
+            foreach (var prop in json.Properties())
+            {
+                if (prop.Value is JValue)
+                {
+                    dict[prefix + prop.Name] = prop.Value.Value<string>();
+                }
+                if (prop.Value is JObject)
+                {
+                    ReadJsonRec((JObject)prop.Value, prefix + prop.Name + ":");
+                }
+            }
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                string val = null;
+                if (dict.TryGetValue(key, out val))
+                    return val;
+                return null;
+            }
+        }
+    }
+
+    public class JsonConfigVariablesBuilder : IConfigVariablesBuilder
+    {
+        private IConfigVariables result = new EmptyConfigVariables();
+
+        public JsonConfigVariablesBuilder UseJsonContent(string jsonContent)
+        {
+            result = new JsonConfigVariables(jsonContent);
+            return this;
+        }
+
+        public JsonConfigVariablesBuilder UseFile(string filePath, bool optional)
+        {
+            if(!File.Exists(filePath))
+            {
+                if (!optional)
+                    throw new InvalidOperationException($"Required file {filePath} not found");
+                return this;
+            }
+
+            var fileContent = File.ReadAllText(filePath);
+            result = new JsonConfigVariables(fileContent);
+            return this;
+        }
+
+        public IConfigVariables Build() => result;
+    }
+
+    public static class Exts
+    {
+        public static ConfigVariablesBuilder Json(this ConfigVariablesBuilder builder, 
+            Action<JsonConfigVariablesBuilder> configAction) {
+
+            var jsonBuilder = builder.AddBuilder(new JsonConfigVariablesBuilder());
+
+            configAction?.Invoke(jsonBuilder);
+
+            return builder;
+        }
+
+        public static ConfigVariablesBuilder Json(this ConfigVariablesBuilder builder,
+            string jsonContent)
+        {
+
+            return builder.Json(b => b.UseJsonContent(jsonContent));
+        }
+
+        public static ConfigVariablesBuilder JsonFile(this ConfigVariablesBuilder builder,
+            string filePath, bool optional = false)
+        {
+
+            return builder.Json(b => b.UseFile(filePath, optional));
+        }
+    }
+}
---
+++ src/test/Confifu.Config.Json.Tests/JsonConfigVariablesTests.cs
@@ -0,0 +1,69 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+
+namespace Confifu.Config.Json.Tests
+{
+    public class JsonConfigVariablesTests
+    {
+        [Fact]
+        public void it_reads_json_property_top_level()
+        {
+            var vars = new JsonConfigVariables("{ \"A\": \"B\" }");
+            vars["A"].ShouldBe("B");
+        }
+
+        [Fact]
+        public void it_returns_null_for_not_existing_keys()
+        {
+            var vars = new JsonConfigVariables("{ \"A\": \"B\" }");
+            vars["B"].ShouldBeNull();
+        }
+
+        [Fact]
+        public void it_returns_nested_json_property_separated_by_commas()
+        {
+            var vars = new JsonConfigVariables("{ \"A\": { \"B\": \"C\"} }");
+
+            vars["A:B"].ShouldBe("C");
+        }
+
+        [Fact]
+        public void it_reads_json_from_file()
+        {
+            var jsonFilePath = Path.GetTempFileName();
+
+            File.WriteAllText(jsonFilePath, "{ \"A\": { \"B\": \"C\"} }");
+
+            var vars = new JsonConfigVariablesBuilder().UseFile(jsonFilePath, false).Build();
+
+            vars["A:B"].ShouldBe("C");
+        }
+
+        [Fact]
+        public void it_fails_when_required_file_not_found()
+        {
+            var jsonFilePath = Path.GetTempFileName();
+            File.Delete(jsonFilePath);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var vars = new JsonConfigVariablesBuilder().UseFile(jsonFilePath, false).Build();
+            });
+        }
+
+
+        [Fact]
+        public void it_does_not_fail_when_optional_file_not_found()
+        {
+            var jsonFilePath = Path.GetTempFileName();
+            File.Delete(jsonFilePath);
+            
+            var vars = new JsonConfigVariablesBuilder().UseFile(jsonFilePath, true).Build();
+        }
+    }
+}
